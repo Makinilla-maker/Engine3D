9ef0607fd83414de2d51294406b2b3c57abceba3
Makinilla-maker <valverdeoriol@gmail.com>
2021-11-29 12:00:07 +0100

    a

--- Engine3D/Core/ComponentCamera.h
+++ Engine3D/Core/ComponentCamera.h
@@ -11,6 +11,24 @@ public:
 
 	ComponentCamera(GameObject* parent);
 
+	virtual ~ComponentCamera();
+
+	bool Update(float dt) override;
+
+	float4x4 ViewMatrixOpenGL() const;
+	float4x4 ProjectionMatrixOpenGL() const;
+
+	void SetAspectRatio(float aspectRatio);
+
+	void StartDraw();
+	void EndDraw();
+
+	void ReGenerateBuffer(int w, int h);
+
+	unsigned int framebuffer;
+	unsigned int texColorBuffer;
+	unsigned int rbo;
+
 	
 
 private:
