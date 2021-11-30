d6da33b733162a08353281f6189f35c57fae5eb6
Ruizo <marc_ruiz1995@hotmail.com>
2021-11-30 12:53:36 +0100

    Created MaterialImporter

--- Engine3D/Core/MaterialImporter.cpp
+++ Engine3D/Core/MaterialImporter.cpp
@@ -1,10 +1,36 @@
 #include "Globals.h"
-#include "Application.h"
-#include "ModuleCamera3D.h"
-#include "ModuleInput.h"
-#include "ModuleEditor.h"
-#include "ComponentTransform.h"
-#include "ComponentMesh.h"
-#include "GameObject.h"
+#include "MaterialImporter.h"
 
+#include "DevIL/include/il.h"
+#include "Devil/include/ilut.h"
+#include <memory>
+#include <vector>
+
+void MaterialImporter::Import(const aiMaterial* material, ComponentMaterial* ourMaterial)
+{
+}
+
+uint64_t MaterialImporter::Save(const ComponentMaterial* ourMaterial, char** fileBuffer)
+{
+	ILuint size;
+	ILubyte* data;
+	ilSetInteger(IL_DXTC_FORMAT, IL_DXT5);// To pick a specific DXT compression use
+	size = ilSaveL(IL_DDS, nullptr, 0); // Get the size of the data buffer
+	if (size > 0) {
+		data = new ILubyte[size]; // allocate data buffer
+		if (ilSaveL(IL_DDS, data, size) > 0)
+		{
+			// Save to buffer with the ilSaveIL function
+			*fileBuffer = (char*)data;
+		}
+
+		RELEASE_ARRAY(data);
+	}
+
+	return uint64_t();
+}
+
+void MaterialImporter::Load(const char* fileBuffer, ComponentMaterial* ourMaterial)
+{
+}
 
--- Engine3D/Core/MaterialImporter.h
+++ Engine3D/Core/MaterialImporter.h
@@ -4,11 +4,12 @@
 #include "Math/float3.h"
 #include "Math/float4x4.h"
 #include "Geometry/Frustum.h"
+#include "ComponentMaterial.h"
 
 
 namespace MaterialImporter
 {
-	void Import(const aiMaterial* material, Material* ourMaterial);
-	uint64 Save(const Material* ourMaterial, char** fileBuffer);
-	void Load(const char* fileBuffer, Material* ourMaterial);
+	void Import(const aiMaterial* material, ComponentMaterial* ourMaterial);
+	uint64 Save(const ComponentMaterial* ourMaterial, char** fileBuffer);
+	void Load(const char* fileBuffer, ComponentMaterial* ourMaterial);
 };
